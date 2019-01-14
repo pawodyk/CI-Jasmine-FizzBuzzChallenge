182ebdfb8b54a9783acf46fc26e1801dc2cf8edd
Pawel <14218944+pwodyk@users.noreply.github.com>
2019-01-14 23:56:16 +0000

    completed the script

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,15 +1,22 @@
 describe("FizzBuzz Challenge", function() {
     describe("Testing the output", function() {
+        //numbers divisible by 3 and 5 :
         it("input 45 should return FizzBuzz", function() {
            expect(fizzBuzz(45)).toBe("FizzBuzz");
         });
-        it("input 9 should return FizzBuzz", function () {
+
+        //numbers divisible by 3 :
+        it("input 9 should return Fizz", function () {
             expect(fizzBuzz(9)).toBe("Fizz");
         });
-        it("input 25 should return FizzBuzz", function () {
+
+        //numbers divisible by 5 :
+        it("input 25 should return Buzz", function () {
             expect(fizzBuzz(25)).toBe("Buzz");
         });
-        it("input 7 should return FizzBuzz", function () {
+
+        //numbers not divisible by 3 and 5 :
+        it("input 7 should return 7", function () {
             expect(fizzBuzz(7)).toBe(7);
         });
     });
